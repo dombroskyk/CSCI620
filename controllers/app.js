a7b20e321a9e7b3cd278028da511c225195fa633
kowzrule <dombroskyk@gmail.com>
2015-10-19 13:51:45 -0400

    made the view templates use inheritance, moved map js to external file, found the root for click events in ArcGIS, set up the base for sql queries

--- controllers/app.js
+++ controllers/app.js
@@ -1,14 +1,55 @@
 'use strict';
 
-var https = require('https'); 
+var https = require('https'),
+    mysql = require('mysql'),
+    config = require('../config');
 
 module.exports = {
 
     home: function (req, res) {
         var plant_name = req.query.plantSearch,
-            state = req.query.stateSearch;
+            state = req.query.stateSearch,
+            mysql_connection = mysql.createConnection({
+                host: config.MYSQL.HOST,
+                port: config.MYSQL.PORT,
+                user: config.MYSQL.USER,
+                password: config.MYSQL.PASS,
+                //database: config.MYSQL.DATABASE
+            });
+            
+        mysql_connection.connect(function(err){
+            if( err ){
+                console.error( 'error connecting: ' + err.stack );
+                return;
+            }
+            console.log( 'connected as id ' + mysql_connection.threadId );
+        });
             
         //plant/state query to db
+        //if someone tries to be smart and input both params, we prefer plant
+        //connection.escape(value) to avoid SQL injection
+        //alternatively (preferred) 
+        //connection.query('SELECT * FROM users WHERE id = ?', [userId], function(err, results) {
+            // ... 
+        //});
+        if( plant_name ){
+            //query plant name
+            /*mysql_connection.query('SELECT plant_name FROM plant WHERE plant.plant_name like \'%?%\'', [plant_name], function(err, results){
+                
+            });*/
+        }else if( state ){
+            //query state name
+            /*mysql_connection.query('SELECT plant_name FROM plant as p, location as l WHERE p.plantID = (SELECT locationID FROM location WHERE location.name like ?)', [state], function(err, results){
+                
+            });*/
+        }else{
+            /*mysql_connection.query('SELECT plant_name FROM plant', function(err, rows, fields){
+               if( err ) throw err; //should find more elegant way to handle these
+               
+               console.log( 'query successful' );
+               
+            });*/
+        }
         res.render('index', {plant_name: plant_name, state: state});
     },
 
---
+++ public/js/arcMap.js
@@ -0,0 +1,64 @@
+//start ArcGIS code 
+var map;
+require([
+	"esri/map", "esri/layers/FeatureLayer", "esri/InfoTemplate",
+	"esri/symbols/SimpleLineSymbol", "esri/symbols/SimpleFillSymbol",
+	"esri/renderers/UniqueValueRenderer", "esri/Color",
+	"dojo/domReady!"],
+	function(Map, FeatureLayer, InfoTemplate, SimpleLineSymbol, SimpleFillSymbol, UniqueValueRenderer, Color) {
+		map = new Map("map", {
+			basemap: "gray",
+			center: [-100, 45],
+			zoom: 4,
+			slider: true
+		});
+	map.on("load", addFeatureLayer);
+	map.graphics.on("click", myGraphicsClickHandler);
+
+	function myGraphicsClickHandler(evt) {
+		alert("User clicked on " + evt.graphic);
+	}
+
+	function addFeatureLayer() {
+		//set the symbol to be an outline of the shape filled with a color
+		var defaultSymbol = new SimpleFillSymbol().setStyle(SimpleFillSymbol.STYLE_NULL);
+		defaultSymbol.outline.setStyle(SimpleLineSymbol.STYLE_NULL);
+
+		//create renderer
+		var renderer = new UniqueValueRenderer(defaultSymbol, "STATE_NAME");
+		var canadaRenderer = new UniqueValueRenderer(defaultSymbol, "NAME");
+
+		//add symbol for each possible value
+		var usa_val_names = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho",
+							"Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi",
+							"Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota",
+							"Ohio","Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont",
+							"Virginia","Washington","West Virginia","Wisconsin","Wyoming"],
+			can_val_names = ["British Columbia","Yukon","Northwest Territories","Alberta","Saskatchewan","Nunavut","Manitoba","Ontario","Quebec","Newfoundland and Labrador","New Brunswick","Nova Scotia"];
+		//assign colors for usa shapes
+		for( i = 0; i < usa_val_names.length; i++ ){
+			var val_name = usa_val_names[i];
+			renderer.addValue(val_name, new SimpleFillSymbol().setColor(new Color([0, 255, 0, 0.5])));
+		}
+		//assign colors for canada shapes
+		for( i = 0; i < can_val_names.length; i++ ){
+			var val_name = can_val_names[i];
+			canadaRenderer.addValue(val_name, new SimpleFillSymbol().setColor(new Color([0, 255, 0, 0.5])));
+		}
+		
+		var featureLayer = new FeatureLayer("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer/1");
+		var canadaFeatureLayer = new FeatureLayer("https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/mapoverlays_political/MapServer/4");
+		//print names to the browser console when we click on the region
+		canadaFeatureLayer.on('click', function(e){
+			console.log(e.graphic.attributes.NAME);
+		});
+		featureLayer.on('click', function(e){
+			console.log(e.graphic.attributes.STATE_NAME);
+		});
+		
+		//add renderers of the shapes to the map object
+		featureLayer.setRenderer(renderer);
+		canadaFeatureLayer.setRenderer(canadaRenderer);
+		map.addLayers([canadaFeatureLayer,featureLayer]);
+	}
+});
